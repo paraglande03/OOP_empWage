885c598bddc0f1d4ec69354f8523eef86982b243
paraglande03 <paraglande03@gmail.com>
2021-02-13 14:50:24 +0530

    ADDED emp check

--- OopEmpWage.java
+++ OopEmpWage.java
@@ -1,22 +1,40 @@
 import java.util.Random;
 
 public class OopEmpWage {
+    static int isPresent = 0;
 
     public static void main(String[] args) {
-        OopEmpWage RANDOMCHECK=new OopEmpWage();
-        RANDOMCHECK.randomcheck();
-
+        dailyWage();
     }
 
-    public void randomcheck(){
-        Random RANDOM=new Random();
-        int random=  RANDOM.nextInt(2);
-        if(random==1){
-            System.out.println("Employee is Present");
+    public static void empcheck() {
+
+        Random RANDOM = new Random();
+        int random = RANDOM.nextInt(2);
+        if (random == 1) {
+            System.out.println("Employee is full time");
+            isPresent = 1;
+        } else {
+            System.out.println("Employee is part time");
+            isPresent = 0;
+
         }
-        else{
-            System.out.println("Employee is Absent");
+    }
+
+    public static void dailyWage() {
+        int wagePerHr = 20;
+        int fullDayHr = 8;
+        empcheck();
+        if(isPresent==1){
+            fullDayHr=8;
         }
+        else {
+            fullDayHr=4;
+        }
+        int dailyWage = wagePerHr*fullDayHr;
+        System.out.println("Daliywage is : "+dailyWage);
+
+
     }
 
 }
