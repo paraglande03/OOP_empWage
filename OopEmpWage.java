ebad48135ca8d7ea19440ff0d53d1b1b43f55700
paraglande03 <paraglande03@gmail.com>
2021-02-13 15:29:18 +0530

    ADDED condition for max working days & hrs

--- OopEmpWage.java
+++ OopEmpWage.java
@@ -12,10 +12,10 @@ public class OopEmpWage {
         Random RANDOM = new Random();
         int random = RANDOM.nextInt(2);
         if (random == 1) {
-            System.out.println("Employee is full time");
+            System.out.println("Employee is working full time today");
             isPresent = 1;
         } else {
-            System.out.println("Employee is part time");
+            System.out.println("Employee is working part time today");
             isPresent = 0;
 
         }
@@ -23,21 +23,34 @@ public class OopEmpWage {
 
     public static void dailyWage() {
         int wagePerHr = 20;
-        int fullDayHr = 8;
+        int fullDayHr = 0;
         int workingDayPerMonth=20;
-        empcheck();
-       switch (isPresent){
-           case 1:
-           fullDayHr=8;
-           break;
-           case 0:
-           fullDayHr=4;
-           break;
-       }
-        int dailyWage = wagePerHr*fullDayHr;
-        System.out.println("Dailywage is : "+dailyWage);
-        int Salary = workingDayPerMonth*dailyWage;
-        System.out.println(Salary+ " : is monthly salary");
+        int workingHrPerMonth=100;
+        int workedDays=0;
+        int workedHrs=0;
+        int totalWage=0;
+        while (workedHrs<workingHrPerMonth && workedDays<workingDayPerMonth) {
+            workedHrs = workedHrs+fullDayHr;
+            workedDays++;
+            switch (isPresent) {
+                case 1:
+                    fullDayHr = 8;
+                    break;
+                case 0:
+                    fullDayHr = 4;
+                    break;
+
+            }
+            empcheck();
+             int dailyWage = wagePerHr * fullDayHr;
+
+            System.out.println("Todays salary is : " + dailyWage);
+             totalWage = totalWage+dailyWage;
+
+
+        }
+
+        System.out.println(totalWage+ " : is total monthly salary");
     }
 
 }
